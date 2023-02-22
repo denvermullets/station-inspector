5241ae32d37265c0b80476921d5578ebe06fc605
denvermullets <devmullets@gmail.com>
2023-02-22 09:31:43 -0500

    feat(csv): add csv route

---
+++ README.md
@@ -0,0 +1,6 @@
+switch to server folder
+yarn knex migrate:make <db_name>
+
+http://localhost:3000/api/v1/csv/generate
+numRows
+numExtraColumns
---
+++ server/src/controllers/csv.controller.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import { Request, Response } from "express";
+import { generateCSV } from "../helpers/generateFile";
+
+export const getCSV = async (req: Request, res: Response) => {
+  const { numRows, numExtraColumns } = req.query;
+
+  try {
+    const csv = generateCSV(
+      Number(numRows || 10),
+      Number(numExtraColumns || 0)
+    );
+
+    fs.writeFileSync("./downloads/carInfo.csv", csv);
+
+    return res.json({ message: "file saved in server/downloads" });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
