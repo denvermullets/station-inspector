51de3aa9847d3bbd783c7c74a83bd5f629a7ec9a
Ryan V <47340962+denvermullets@users.noreply.github.com>
2023-02-23 08:08:56 -0500

    feat(ingest): add route to upload csv (#2)
    * feat(ingest): add route to upload csv
    
    * chore(license): add license to remove warnings

---
+++ server/src/controllers/ingest.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import csv from "csv-parser";
+
+export const ingestCSV = async (req: Request, res: Response) => {
+  const { file } = req;
+
+  if (!file?.path) {
+    return res.status(400).send("Missing required file properties");
+  }
+
+  try {
+    fs.createReadStream(file.path)
+      .pipe(csv())
+      .on("data", (data) => {
+        console.log("this is data ======>", data);
+      })
+      .on("end", () => {
+        console.log("CSV file is processed");
+      });
+
+    return res.json({ message: "file successfully uploaded" });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
